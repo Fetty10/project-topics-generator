4e0c0904745df82054a3338ab0ff7eb6c91f25a1
Faith Osabutey <osabuteyfaith@gmail.com>
2024-11-11 00:46:17 +0000

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayTipics(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "✍🏽",
-    delay: 80,
+    delay: 50,
   });
 }
 
@@ -12,12 +12,14 @@ function generateTopics(event) {
 
   let userInput = document.querySelector("#text-input");
   let apiKey = "o74291bedbb48f3c3et5a5a6ad40853a";
-  let prompt = `Generate academic research topics based on ${userInput.value}, make it precise with no further explanation, add fullstop at the end of every topic and seperate each new number with a <br />. Sign the topics with By Fetty inside a <strong> element at the end of the topics`;
+  let prompt = `Generate academic research topics based on ${userInput.value}, make it precise with no further explanation, add fullstop at the end of every topic and seperate each new number with a <br />. Do not write more than the number entered by the student. Sign the topics with By Fetty AI inside a <strong> element at the end of the topics with a <br />`;
   let context =
     "You are and ai academic researcher, please provide the exact number of topics requested by the user";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   let topicElement = document.querySelector("#project-topics");
-  topicElement.innerHTML = "Generating your topic(s)... please wait";
+  topicElement.classList.remove("hidden");
+  topicElement.innerHTML = "📚 Generating your topics... please wait";
   axios.get(apiUrl).then(displayTipics);
 }
 
